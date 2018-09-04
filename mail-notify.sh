34a3b948940e5899efed28c00b66aa066aca7020
Wei Liu <liuw@liuw.name>
2018-09-04 10:46:40 +0100

    Add comments to mail-notify.sh

--- mail-notify.sh
+++ mail-notify.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# Feed the entire email to this script, it will extract Subject and From.
+
 while IFS= read -r line; do
 	#header="$header$line\n"
 	if [[ x$line = "xSubject:"* ]]; then
@@ -10,6 +12,7 @@ while IFS= read -r line; do
 	fi
 done
 
+# No need to notify me on mail from myself
 case $from in
 	*"Wei Liu"*) exit 0;;
 esac
